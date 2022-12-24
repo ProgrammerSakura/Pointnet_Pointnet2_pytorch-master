5894774836ec0bc7ba897da6c68b0193a9b775ab
wuka <1649422875@qq.com>
2022-12-24 21:56:54 +0800

    add comments

--- models/pointnet_utils.py
+++ models/pointnet_utils.py
@@ -41,7 +41,7 @@ class STN3d(nn.Module):
         x = self.fc3(x)
 
         # iden生成单位变换矩阵
-        # repeat 表示复制
+        # repeat 表示复制，from_numpy表示转换为tensor类型数据。
         iden = Variable(torch.from_numpy(np.array([1, 0, 0, 0, 1, 0, 0, 0, 1]).astype(np.float32))).view(1, 9).repeat(
             batchsize, 1)
         if x.is_cuda:
